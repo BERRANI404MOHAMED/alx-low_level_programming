8731e042e465b48be541998817039be7208ff013
BERRANI404MOHAMED <berranimohamed7@gmail.com>
2023-03-16 17:39:42 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,3 +3,8 @@
  * main - a function that prints a given input within puts
  * Return: 0 - (Success)
  */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
